e5b6fae236711043d2570c19badfcf0c2bc5c97c
MichalJ <rekwizytornia@hotmail.com>
2024-05-17 23:45:30 +0200

    dodanie chmurki 17:37

---
+++ game.py
@@ -0,0 +1,25 @@
+import pygame, sys
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption("Cat Hunter")
+        self.screen = pygame.display.set_mode((640, 480))
+
+        self.clock = pygame.time.Clock()
+
+        self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+
+    def run(self):
+        while True:
+            self.screen.blit(self.img, (100, 200))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.quit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
