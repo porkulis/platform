d7bce7f6051965adb593c694a7d0813b35dcdfde
MichalJ <rekwizytornia@hotmail.com>
2024-05-19 00:04:30 +0200

    Dodanie ruchu gora/dol 27:12

--- game.py
+++ game.py
@@ -10,14 +10,31 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+        self.img.set_colorkey((0, 0, 0)) #ustawienie koloru tła obrazka
+
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
 
     def run(self):
         while True:
-            self.screen.blit(self.img, (100, 200))
+            self.screen.fill((50, 150, 230))
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.screen.blit(self.img, self.img_pos)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
 
             pygame.display.update()
             self.clock.tick(60)
